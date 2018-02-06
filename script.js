06128410fdedbd4ca38d74ae9854bf8da720a304
Ariel Sansone <32747901+psaxone@users.noreply.github.com>
2018-02-06 20:09:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    name: "Pablo Ariel Sansone",
+    bio: "Front end developer and much more",
+    proyectos: [
+      {
+        text: "Buenos Aires Free Wifi Spots",
+        url: "https://psaxone.github.io/WiFi-Spots/"
+      },
+      { text: "News Search App", url: "https://psaxone.github.io/news/" },
+      { text: "Weather App", url: "https://psaxone.github.io/weather/" }
+    ]
+  }
+});
